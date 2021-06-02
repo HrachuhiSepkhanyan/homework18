3c95f4d8a65a9a236c12d451d97801abc78e2e32
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2021-06-02 14:03:41 +0400

    the sum of a given array.

---
+++ sumRecursion.js
@@ -0,0 +1,15 @@
+let sum = function(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+
+    function add(array, i) {
+        // console.log(array[i]);
+        if (i === array.length - 1) {
+            return array[i];
+        }
+        return array[i] + add(array, i + 1);
+    }
+    return add(array, 0);
+};
+console.log(sum([1, 2, 3, 4, 5, 6]))
